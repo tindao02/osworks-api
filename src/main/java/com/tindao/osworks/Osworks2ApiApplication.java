1902b1e4afcb3c75b2628eea855cb1ed43c8665e
Manoel Tindão <timdao02@hotmail.com>
2020-05-15 00:10:01 -0300

    Criação do projeto AlgaWorks

---
+++ src/main/java/com/tindao/osworks/Osworks2ApiApplication.java
@@ -0,0 +1,13 @@
+package com.tindao.osworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Osworks2ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Osworks2ApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tindao/osworks/Osworks2ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tindao.osworks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Osworks2ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
